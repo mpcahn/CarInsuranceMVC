94effd18258526d3c5c3320ae86698d236089a4d
mpcahn <mpcahn@gmail.com>
2018-11-05 22:39:44 -0700

    Add db, and begin logic on HomeController

--- CarInsuranceMVC/Controllers/HomeController.cs
+++ CarInsuranceMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarInsuranceMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,38 +8,98 @@ using System.Web.Mvc;
 namespace CarInsuranceMVC.Controllers
 {
     public class HomeController : Controller
-    {
-        //Use the following rules to calculate a quote:
-
-        //Start with a base of $50 / month.
-
-        //If the user is under 25, add $25 to the monthly total.
-
-        //If the user is under 18, add $100 to the monthly total.
-
-        //If the user is over 100, add $25 to the monthly total.
-
-        //If the car's year is before 2000, add $25 to the monthly total.
-
-        //If the car's year is after 2015, add $25 to the monthly total.
-
-        //If the car's Make is a Porsche, add $25 to the price.
-
-        //If the car's Make is a Porsche and its model is a 911 Carrera, add an additional $25 to the price.
-
-        //Add $10 to the monthly total for every speeding ticket the user has.
-
-        //If the user has ever had a DUI, add 25% to the total.
-
-        //If it's full coverage, add 50% to the total.
-
-
-        //There must also be an Admin view for a site administrator. This page must:
-
-        //Show all quotes issued, along with the user's first name, last name, and email address.
+    {        
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetQuote(
+            string firstName,
+            string lastName,
+            string emailAddress,
+            string DOB,
+            string carYear,
+            string carMake,
+            string carModel,
+            string DUI,
+            string speedingTickets,
+            string coverage
+            )
+        {
+            if (
+                string.IsNullOrEmpty(firstName)       ||
+                string.IsNullOrEmpty(lastName)        ||
+                string.IsNullOrEmpty(emailAddress)    ||
+                string.IsNullOrEmpty(DOB)             ||
+                string.IsNullOrEmpty(carYear)         ||
+                string.IsNullOrEmpty(carMake)         ||
+                string.IsNullOrEmpty(carModel)        ||
+                string.IsNullOrEmpty(DUI)             ||
+                string.IsNullOrEmpty(speedingTickets) ||
+                string.IsNullOrEmpty(coverage)
+                )
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                //Use the following rules to calculate a quote:
+                //Start with a base of $50 / month.
+                var quote = 50;
+
+                //If the user is under 25, add $25 to the monthly total.
+                
+
+                //If the user is under 18, add $100 to the monthly total.
+
+                //    *
+                //If the user is over 100, add $25 to the monthly total.
+
+                //    *
+                //If the car's year is before 2000, add $25 to the monthly total.
+                //    *
+                //If the car's year is after 2015, add $25 to the monthly total.
+                //    *
+                //If the car's Make is a Porsche, add $25 to the price.
+                //    *
+                //If the car's Make is a Porsche and its model is a 911 Carrera, add an additional $25 to the price.
+                //    *
+                //Add $10 to the monthly total for every speeding ticket the user has.
+
+                //    *
+                //If the user has ever had a DUI, add 25 % to the total.
+
+                //    *
+                //If it's full coverage, add 50% to the total.
+
+
+
+                bool dui = false;
+                if (DUI == "y")
+                {
+                    dui = true;
+                }
+                                
+
+                using (CarInsuranceEntities db = new CarInsuranceEntities())
+                {
+                    var quote = new QuoteInfo();
+                    quote.FirstName = firstName;
+                    quote.LastName = lastName;
+                    quote.EmailAddress = emailAddress;
+                    quote.DOB = Convert.ToDateTime(DOB);
+                    quote.CarYear = Convert.ToInt32(carYear);
+                    quote.CarMake = carMake;
+                    quote.CarModel = carModel;
+                    quote.DUI = dui;
+                    quote.SpeedingTicket = Convert.ToInt32(speedingTickets);
+                    quote.coverage = coverage;
+                }
+
+                return View("Success");
+            }
         }
 
         public ActionResult About()
