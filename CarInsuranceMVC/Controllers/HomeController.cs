4491b8f226c1a4864d68bb88a8d870581cdd304a
mpcahn <mpcahn@gmail.com>
2018-11-05 19:55:21 -0700

    Add project files.

---
+++ CarInsuranceMVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsuranceMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Use the following rules to calculate a quote:
+
+        //Start with a base of $50 / month.
+
+        //If the user is under 25, add $25 to the monthly total.
+
+        //If the user is under 18, add $100 to the monthly total.
+
+        //If the user is over 100, add $25 to the monthly total.
+
+        //If the car's year is before 2000, add $25 to the monthly total.
+
+        //If the car's year is after 2015, add $25 to the monthly total.
+
+        //If the car's Make is a Porsche, add $25 to the price.
+
+        //If the car's Make is a Porsche and its model is a 911 Carrera, add an additional $25 to the price.
+
+        //Add $10 to the monthly total for every speeding ticket the user has.
+
+        //If the user has ever had a DUI, add 25% to the total.
+
+        //If it's full coverage, add 50% to the total.
+
+
+        //There must also be an Admin view for a site administrator. This page must:
+
+        //Show all quotes issued, along with the user's first name, last name, and email address.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CarInsuranceMVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CarInsuranceMVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarInsuranceMVC")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("399c0a51-2d27-4eaa-8ad3-12d5cd8309eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
